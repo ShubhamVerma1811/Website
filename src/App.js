18e43eadcab424086d13d2d3c348c6938a2dc3ed
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-05-22 18:46:36 +0530

    Created Hero Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import Hero from './components/Hero';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <nav className="flex items-center justify-between flex-wrap p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <img
+            src="https://cdn.dribbble.com/users/1222918/screenshots/4495723/s1d.jpg"
+            alt="logo"
+            className="h-12"
+          />
+        </div>
+        <div className="block lg:hidden">
+          <button
+            type="submit"
+            className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          >
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+      </nav>
+      <Hero />
+    </div>
   );
 }
 
---
+++ src/components/Hero.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+function Hero() {
+  const URI = 'https://gitconnected.com/v1/portfolio/shubhamverma1811';
+  const [data, setData] = useState({});
+  const {
+    basics, skills, projects, work,
+    } = data;
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(URI);
+      const receivedData = await res.json();
+      setData(receivedData);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {basics && (
+        <>
+          <h1 className="text-6xl">{data.basics.name}</h1>
+          <h1 className="text-5xl">{data.basics.label}</h1>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Hero;
