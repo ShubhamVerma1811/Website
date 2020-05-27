57ec6ba5e06f809674eb082adf8f94d62d6b3769
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-05-27 12:38:49 +0530

    Contact Comp Done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Contact() {
   return (
     <section id="contact" className="text-gray-700 body-font relative">
-      <div className="container px-5 py-24 mx-auto flex sm:flex-no-wrap flex-wrap">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 flex justify-center pt-24">
+        Contact Us
+      </h1>
+      <div className="container px-5 py-24 mx-auto flex sm:flex-no-wrap flex-wrap pt-0">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             className="absolute inset-0"
@@ -12,7 +15,7 @@ function Contact() {
             marginHeight="0"
             marginWidth="0"
             scrolling="no"
-            src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+            src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=Kothapet&ie=UTF8&t=&z=14&iwloc=B&output=embed"
             width="100%"
             height="100%"
             frameBorder="0"
@@ -22,10 +25,7 @@ function Contact() {
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm">
                 ADDRESS
               </h2>
-              <p className="leading-relaxed">
-                Photo booth tattooed prism, portland taiyaki hoodie neutra
-                typewriter
-              </p>
+              <p className="leading-relaxed">Hyderabad, TS India</p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm">
@@ -35,47 +35,54 @@ function Contact() {
                 href="mailto:imshubamverma.sv@gmail.com"
                 className="text-indigo-500 leading-relaxed"
               >
-                imshubamverma.sv@gmail.com
+                hi@shubhamverma.me
               </a>
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <a href="tel:+918096625858">
+                <p className="leading-relaxed">+91 80966 25858</p>
+              </a>
             </div>
           </div>
         </div>
-        <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+        <form
+          id="contact-form"
+          name="contact"
+          method="POST"
+          action="https://formspree.io/imshubhamverma.sv@gmail.com"
+          className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
           <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-            Feedback
+            Let&apost;s Chat
           </h2>
           <p className="leading-relaxed mb-5 text-gray-600">
-            Post-ironic portland shabby chic echo park, banjo fashion axe
+            Have something to say?
           </p>
           <input
             className="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
             placeholder="Name"
             type="text"
+            name="Name"
           />
           <input
             className="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
             placeholder="Email"
             type="email"
+            name="Email"
           />
           <textarea
             className="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-indigo-500 text-base px-4 py-2 mb-4 resize-none"
             placeholder="Message"
+            name="Message"
           />
           <button
             type="submit"
             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
           >
-            Button
+            Send Message
           </button>
-          <p className="text-xs text-gray-500 mt-3">
-            Chicharrones blog helvetica normcore iceland tousled brook viral
-            artisan.
-          </p>
-        </div>
+        </form>
       </div>
     </section>
   );
