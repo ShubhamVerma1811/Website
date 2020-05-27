362e7a8df4cfe97558cfdd494f3d37d312c1b4b4
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-05-27 12:39:54 +0530

    Implemented API data to display projects

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,30 +4,35 @@ import { ApiDataContext } from '../context/ApiDataContext';
 function Gallery() {
   const [data, setData] = useContext(ApiDataContext);
   const { projects } = data;
-  const { images } = projects;
+
+  console.log(projects[0].images);
 
   return (
     <section className="text-gray-700 body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-            Master Cleanse Reliac Heirloom
+            Projects
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
             gentrify, subway tile poke farm-to-table. Franzen you probably
-            haven't heard of them man bun deep jianbing selfies heirloom.
+            haven&apos;t heard of them man bun deep jianbing selfies heirloom.
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
-            <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div
+              key={project.name}
+              className="lg:w-1/3 sm:w-1/2 p-4 overflow-hidden"
+            >
               <div className="flex relative">
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
-                  src="https://dummyimage.com/600x360"
+                  src={require('../assets/unsplash.png')}
                 />
+                )
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
                     {project.name}
