e6ede5e538445440f03409ef4d462ad7819397a9
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-05-27 17:32:38 +0530

    Added Live and Repo Buttons,Project Summary, Languages and Libraries Used

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,38 +1,35 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React, { useContext } from 'react';
 import { ApiDataContext } from '../context/ApiDataContext';
 
 function Gallery() {
-  const [data, setData] = useContext(ApiDataContext);
+  const data = useContext(ApiDataContext);
   const { projects } = data;
-
-  console.log(projects[0].images);
 
   return (
     <section className="text-gray-700 body-font">
-      <div className="container px-5 py-24 mx-auto">
+      <div className="container px-5 py-12 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
             Projects
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-            gentrify, subway tile poke farm-to-table. Franzen you probably
-            haven&apos;t heard of them man bun deep jianbing selfies heirloom.
+            Here is the showcase of some of my featured projects.
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
-            <div
-              key={project.name}
-              className="lg:w-1/3 sm:w-1/2 p-4 overflow-hidden"
-            >
+            <div key={project.name} className="lg:w-1/3 sm:w-1/2 p-4">
               <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={require('../assets/unsplash.png')}
-                />
-                )
+                {project.images.length > 0
+                  && project.images.map((image) => (
+                    <img
+                      alt="gallery"
+                      className="absolute inset-0 w-full h-full object-cover object-center"
+                      src={image.resolutions.mobile.url}
+                    />
+                  ))}
+
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
                     {project.name}
@@ -40,12 +37,41 @@ function Gallery() {
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                     {project.displayName}
                   </h1>
-                  <p className="leading-relaxed">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Nam corporis a laborum nostrum vel, accusantium quam
-                    exercitationem aliquam perferendis odit veniam iste
-                    obcaecati animi quas.
-                  </p>
+                  <p className="leading-relaxed">{project.summary}</p>
+                  <div className="flex">
+                    <div>
+                      <p className="text-indigo-500">Languages Used : </p>
+                      {project.languages.map((lang) => (
+                        <p className="leading-relaxed inline pr-3">{lang}</p>
+                      ))}
+                    </div>
+                    {project.libraries.length > 0 && (
+                      <div>
+                        <p className="text-indigo-500">Libraries Used : </p>
+                        {project.libraries.map((lib) => (
+                          <p className="leading-relaxed inline pr-4">{lib}</p>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                  <div className="flex justify-start items-center">
+                    <button
+                      type="button"
+                      className="mt-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-500 hover:text-white rounded text-lg"
+                    >
+                      <a href={project.website} target="_blank">
+                        Live Demo
+                      </a>
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-4 ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-2 focus:outline-none hover:bg-gray-300 rounded text-lg hover:bg-indigo-500 hover:text-white"
+                    >
+                      <a href={project.githubUrl} target="_blank">
+                        GitHub Repo
+                      </a>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
