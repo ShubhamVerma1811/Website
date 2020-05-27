ae9de08f0e4fe6c14c1e92f1a799568689510e14
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-05-27 12:38:27 +0530

    Hero Comp Done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useContext } from 'react';
+import { Link } from 'react-scroll';
 import { ApiDataContext } from '../context/ApiDataContext';
 
 const styles = {
@@ -14,32 +15,39 @@ function Hero() {
     <section className="text-gray-700 body-font">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          {/* <img
+          <img
             className="object-cover object-center rounded"
             alt="hero"
             src={basics.picture}
-          /> */}
+          />
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Hi, I am Shubham Verma
-            <br className="hidden lg:inline-block" />
+            Hi, I am
+            {` ${basics.name}`}
+            <br />
             {basics.label.toUpperCase()}
           </h1>
           <p className="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-            plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-            mlkshk tote bag selvage hot chicken authentic tumeric truffaut
-            hexagon try-hard chambray.
+            I am a Front End Developer with knowledge of HTML, CSS, JS and React.
+            I have been learning Back End Development and tinkering with Competitive Programming.
           </p>
           <div className="flex justify-center">
-            <button type="submit" className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <button
+              type="submit"
+              className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
               <a href="resume.pdf" download="Resume-Shubham Verma">
                 Download Resume
               </a>
             </button>
-            <button type="submit" className="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">
-              Contact
+            <button
+              type="submit"
+              className="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg"
+            >
+              <Link to="contact" smooth duration={1000}>
+                Contact
+              </Link>
             </button>
           </div>
         </div>
