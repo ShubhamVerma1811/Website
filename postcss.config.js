86affd04b3190ee303be64c19ec470d21bec38ef
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-06-08 16:21:44 +0530

    added purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   plugins: [
-    // ...
-    require("tailwindcss"),
-    require("autoprefixer"),
-    // ...
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./pages/**/*.js', './components/**/*.js'],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ],
 };
