b8bc741b443621eab375d781040744b86dcf0983
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-06-08 16:36:36 +0530

    purge prod

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,9 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')({
-      content: ['./pages/**/*.js', './components/**/*.js'],
-      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    }),
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+        content: ['./pages/**/*.js', './components/**/*.js'],
+        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
   ],
 };
