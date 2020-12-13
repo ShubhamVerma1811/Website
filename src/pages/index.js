042aeac908612ec4967fdd18336e6772f2217356
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-13 12:55:40 +0530

    removed comtact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { Fragment } from 'react';
 import Blogs from '../components/Blogs';
-import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import Gallery from '../components/Gallery';
 import Header from '../components/Header';
