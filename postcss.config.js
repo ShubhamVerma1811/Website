0b5da2a17e7ec6b081e0b7926f91842d907149b5
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-09-14 08:36:45 +0530

    fix tw purge

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
     require('autoprefixer'),
     process.env.NODE_ENV === 'production' &&
       require('@fullhuman/postcss-purgecss')({
-        content: ['./src/**/*.js'],
+        content: ['./src/**/*.{js,ts,jsx,tsx}'],
         defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
       }),
   ],
