5f0caadde8eef564336fc4b0bc33b54dfb1dcb3a
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-02-05 09:35:17 +0530

    captions

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -35,7 +35,17 @@ const Blog = (props: IBlog) => {
           ],
           rehypeHighlight,
         ]}
-        className="prose text-white prose-headings:text-white prose-a:text-white prose-blockquote:text-white prose-code:text-white prose-ul:text-white prose-li:text-white">
+        className="prose text-white prose-headings:text-white prose-a:text-white prose-blockquote:text-white prose-code:text-white prose-ul:text-white prose-li:text-white"
+        components={{
+          img: (props: any) => {
+            return (
+              <>
+                <img {...props} className="my-0" />
+                <small>{props.alt}</small>
+              </>
+            );
+          },
+        }}>
         {props.md}
       </ReactMarkdown>
     </div>
