4645cd7d898105fc54135709b31ca37d2040ffae
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-02-14 06:10:05 +0530

    fix: state for views

--- src/pages/api/views.ts
+++ src/pages/api/views.ts
@@ -5,8 +5,8 @@ const notion = new Notion();
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const { page_id, views } = JSON.parse(req.body);
-    await notion.updateViews(page_id, views);
-    res.send('OK');
+    const view = await notion.updateViews(page_id, views);
+    res.send(view);
   } else res.send('Request method is not supported');
 };
 
