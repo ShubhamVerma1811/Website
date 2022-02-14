81872a86b3b80777a1f34229e0c09590f413a52b
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-02-14 08:28:52 +0530

    Fix views on load (#15)
    * bump: tailwind v3 and class sort
    
    * chore: blogs integraion
    
    * fix: prettier
    
    * fix: caption null check
    
    * tailwind typegraphy
    
    * remark
    
    * fix types
    
    * GFM
    
    * more work
    
    * rm packes
    
    * captions
    
    * remove code backticks
    
    * styles
    
    * tags and date aside
    
    * tag and styles
    
    * chnage font
    
    * added code highlight and styles
    
    * added back to top on scroll
    
    * blog page styling
    
    * fix: null check
    
    * add head title
    
    * feat: find by slug
    
    * fix: revalidate on 404
    
    * fix: publish date
    
    * added blogs to home and modify hero
    
    * tags page and slug rename
    
    * fix: query for tags
    
    * chore: add copy code element
    
    * fix: layout
    
    * feat: added views count
    
    * fix: state for views
    
    * fix: views from revalidate
    
    * fix: views latency
    
    * fix: crash

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -52,17 +52,17 @@ const Blog = (props: IBlog) => {
   ];
 
   useEffect(() => {
+    setViews(props.blogInfo?.properties?.views?.number);
+  }, [props.blogInfo?.properties?.views?.number]);
+
+  useEffect(() => {
     async function views() {
-      const res = await fetch('/api/views', {
+      await fetch('/api/views', {
         method: 'POST',
         body: JSON.stringify({
           page_id: props?.blogInfo?.id,
         }),
       });
-
-      const _views = await res.json();
-
-      setViews(_views);
     }
 
     views();
