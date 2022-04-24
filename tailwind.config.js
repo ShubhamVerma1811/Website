bd5fdf46c58d82b221549ee1b30cb517dfee6b26
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-04-24 16:41:03 +0530

    chore: tailwind theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,48 @@
+/**
+ * @type {import('tailwindcss/tailwind-config').TailwindConfig}
+ */
 module.exports = {
   content: ['./src/**/*.{ts,tsx,html}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          primary: 'var(--color-primary)',
+          secondary: 'var(--color-secondary)',
+          accent: 'var(--color-accent)',
+          'primary-muted': 'var(--color-primary-muted)',
+          'secondary-muted': 'var(--color-secondary-muted)',
+        },
+      },
+      backgroundColor: (theme) => {
+        return {
+          skin: {
+            primary: 'var(--color-primary)',
+            secondary: 'var(--color-secondary)',
+            accent: 'var(--color-accent)',
+            'primary-muted': (props) => {
+              return 'var(--color-primary-muted)';
+            },
+            'secondary-muted': 'var(--color-secondary-muted)',
+          },
+        };
+      },
+      borderColor: (theme) => {
+        return {
+          skin: {
+            primary: 'var(--color-primary)',
+            secondary: 'var(--color-secondary)',
+            accent: 'var(--color-accent)',
+            'primary-muted': (props) => {
+              return 'var(--color-primary-muted)';
+            },
+            'secondary-muted': 'var(--color-secondary-muted)',
+          },
+        };
+      },
+    },
   },
+
   plugins: [require('@tailwindcss/typography')],
 };
