6996621fb3e980c98f6a48fef7c4732ad2d68599
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-07-31 08:19:04 +0530

    fix: use sanity CDN on prod

--- src/services/sanity-config.ts
+++ src/services/sanity-config.ts
@@ -1,7 +1,7 @@
 export const sanityConfig = {
   dataset: process.env.SANITY_DATASET || 'dev',
   projectId: process.env.SANITY_PROJECT_ID,
-  useCdn: process.env.NODE_ENV !== 'production',
+  useCdn: process.env.NODE_ENV === 'production',
   apiVersion: '2021-03-25',
   token: process.env.SANITY_API_TOKEN
 };
