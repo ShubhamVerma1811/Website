a84ef0b077b73a587b26fb08c5fecd97dd75e5b7
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-10-16 18:14:20 +0530

    feat: add monday's page

--- src/pages/monday.tsx
+++ src/pages/monday.tsx
@@ -2,34 +2,34 @@ import React from 'react';
 import { PageLayout } from 'layouts';
 import Head from 'next/head';
 
-const Friday = () => {
-  const AAJ_FRIDAY_HAI = new Date().getDay() === 5;
+const Monday = () => {
+  const AAJ_MONDAY_HAI = new Date().getDay() === 1;
 
   return (
     <React.Fragment>
       <Head>
-        <title>FRIDAY AAAAAA!</title>
+        <title>Monday ki BT HO GAYI</title>
         <meta
           name='description'
-          content='Keda kaabu ni husan te paave, Dil seene to nikalda jaave, batti bor billo do nain kaale'
+          content='Abey yaar! BT ho gayi'
         />
       </Head>
       <PageLayout>
         <div className='flex-row items-center justify-center'>
-          {!AAJ_FRIDAY_HAI ? (
+          {!AAJ_MONDAY_HAI ? (
             <div>
-              <h1 className='text-4xl font-bold text-skin-secondary md:text-8xl'>
-                AAJ FRIDAY NAHI HAI
+              <h1 className='text-3xl font-bold text-skin-secondary md:text-8xl'>
+                AAJ MONDAY NAHI HAI
               </h1>
               <p className='text-skin-secondary'>
-                Come back on Friday for a treat.
+                Come back on Monday for BT.
               </p>
             </div>
           ) : (
             <iframe
               className='h-[703px] w-full'
-              src='https://www.youtube-nocookie.com/embed/ah8StqTM7hg?controls=0'
-              title='Aaj Friday hai'
+              src='https://www.youtube-nocookie.com/embed/ybRYNRcIuA4?controls=0&amp;start=76&end=79'
+              title='BT Ho Gayi | Hustle 2.0'
               allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'></iframe>
           )}
         </div>
@@ -38,4 +38,4 @@ const Friday = () => {
   );
 };
 
-export default Friday;
+export default Monday;
