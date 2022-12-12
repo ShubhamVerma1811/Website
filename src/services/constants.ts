99f4b63cb84d75cc4b2c4f87dcf1e255fc4af4df
Shubham Verma <shubhamv@geekyants.com>
2022-12-12 20:40:18 +0530

    fix: remove defined outline for better a11y

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -12,4 +12,4 @@ export const SPOTIFY_URL = 'https://shbm.fyi/sp';
 export const CAL_URL = 'https://shbm.fyi/cal';
 export const CURRENT_ORGANIZATION = 'GeekyAnts';
 export const CURRENT_TITLE = 'Software Engineer - III';
-export const RESUME_URL = 'https://shbm.fyi/resume'
+export const RESUME_URL = 'https://shbm.fyi/resume';
