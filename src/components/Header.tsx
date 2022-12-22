6cba7fbddf22060e7573d7be29a4bc4b4d197adb
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-12-22 10:38:09 +0530

    chore: move socials to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
+import { GithubIcon, LinkedInIcon, TwitterIcon } from 'components/Icons';
 import { useAtom } from 'jotai';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { GITHUB_URL, LINKEDIN_URL, TWITTER_URL } from 'services/constants';
 import { isDarkModeAtom } from 'store/atoms/theme';
 import { Moon, Sun } from './Icons';
 
@@ -27,7 +29,7 @@ export const Header = () => {
 
   return (
     <header className='my-4 mb-12 rounded-md bg-skin-primary' id='header'>
-      <nav className='flex'>
+      <nav className='flex items-center'>
         <div>
           {headerLinks.map((link, index) => {
             return (
@@ -43,15 +45,38 @@ export const Header = () => {
           })}
         </div>
         <div className='ml-auto'>
-          <button
-            role='button'
-            aria-label='Toggle dark mode'
-            className='rounded-md border-2 border-skin-secondary bg-skin-primary p-2 hover:transition-all'
-            onClick={() => {
-              setIsDarkMode((prev) => !prev);
-            }}>
-            {isDarkMode ? <Moon /> : <Sun />}
-          </button>
+          <div className='flex flex-wrap items-center'>
+            <a
+              target='_blank'
+              href={TWITTER_URL}
+              className=' umami--click--hero-twitter mr-1 w-max rounded-md p-2 text-lg text-[#1DA1F2] hover:bg-skin-secondary-muted'
+              rel='noopener noreferrer'>
+              <TwitterIcon className='inline' />
+            </a>
+            <a
+              target='_blank'
+              href={GITHUB_URL}
+              className='umami--click--hero-github mr-1 w-max rounded-md p-2 text-lg text-skin-secondary hover:bg-skin-secondary-muted'
+              rel='noopener noreferrer'>
+              <GithubIcon />
+            </a>
+            <a
+              target='_blank'
+              href={LINKEDIN_URL}
+              className='umami--click--hero-linkedin w-max rounded-md p-2 text-lg text-[#0a66c2] hover:bg-skin-secondary-muted dark:text-[#ffffffe6]'
+              rel='noopener noreferrer'>
+              <LinkedInIcon />
+            </a>
+            <button
+              role='button'
+              aria-label='Toggle dark mode'
+              className='ml-1 rounded-md p-2 hover:bg-skin-secondary-muted'
+              onClick={() => {
+                setIsDarkMode((prev) => !prev);
+              }}>
+              {isDarkMode ? <Moon /> : <Sun />}
+            </button>
+          </div>
         </div>
       </nav>
     </header>
