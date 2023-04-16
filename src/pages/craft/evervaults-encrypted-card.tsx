732c4f15d07ce586b7a0484585e9bddb024adbfa
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2023-04-16 07:52:15 +0530

    fix: evervalut card styles

--- src/pages/craft/evervaults-encrypted-card.tsx
+++ src/pages/craft/evervaults-encrypted-card.tsx
@@ -2,50 +2,36 @@ import { PageLayout } from 'layouts';
 import { MetaLayout } from 'layouts/MetaLayout';
 import { useRef, useState } from 'react';
 
+function generateFakeEncryptedString(length: number) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#@$%&*';
+  const charactersLength = characters.length;
+  let result = '';
+  let counter = 0;
+
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+
+  return result;
+}
+
 const Card = () => {
-  const [text, setText] = useState(
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, errorLorem ipsum dolor sit amet consectetur adipisicing elit. In, fuga! Consequatur, minus? Eum aliquam quas similique cum doloremque ea voluptatem explicabo odit maxime, error asperiores ratione debitis aperiam consequuntur vitae hic quae asperiores ratione debitis aperiam consequuntur vitae hic quae asperiores ratione debitis aperiam consequuntur vitae hic quaerat ipsa.'
-  );
-
-  function generateFakeEncryptedText(plaintext: string) {
-    const key = 'abcdefghijklmnopqrstuvwxyz'; // the substitution key
-    const shuffledKey = shuffleKey(key); // shuffle the key randomly
-    let ciphertext = '';
-
-    // iterate through each character in the plaintext
-    for (let i = 0; i < plaintext.length; i++) {
-      const plaintextChar = plaintext.charAt(i).toLowerCase();
-      let ciphertextChar = plaintextChar;
-
-      // if the character is a letter, substitute it using the key
-      if (/[a-z]/i.test(plaintextChar)) {
-        const keyIndex = key.indexOf(plaintextChar);
-        ciphertextChar = shuffledKey.charAt(keyIndex);
-      }
-
-      ciphertext += ciphertextChar;
-    }
-
-    return ciphertext;
-  }
-
-  function shuffleKey(key: string) {
-    let shuffledKey = '';
-
-    // convert the key to an array and shuffle it using the Fisher-Yates algorithm
-    const keyArray = key.split('');
-    for (let i = keyArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [keyArray[i], keyArray[j]] = [keyArray[j], keyArray[i]];
-    }
-
-    // convert the shuffled array back to a string
-    shuffledKey = keyArray.join('');
-
-    return shuffledKey;
-  }
+  const [text, setText] = useState(generateFakeEncryptedString(10 * 1024));
 
   const textRef = useRef<HTMLParagraphElement>(null);
+
+  function applyStyles(bg: string, clip: string, color: string) {
+    if (!textRef.current) {
+      return;
+    }
+    textRef.current.style.background = bg;
+    // @ts-ignore
+    textRef.current.style['-webkit-background-clip'] = clip;
+    // @ts-ignore
+    textRef.current.style['-webkit-text-fill-color'] = color;
+  }
 
   function relativeCoords(e: any) {
     var bounds = e.target.getBoundingClientRect();
@@ -59,7 +45,7 @@ const Card = () => {
       return;
     }
 
-    setText(generateFakeEncryptedText(text));
+    setText(generateFakeEncryptedString(10 * 1024));
     const { x, y } = relativeCoords(e);
     const bounds = e.currentTarget.getBoundingClientRect();
 
@@ -70,25 +56,39 @@ const Card = () => {
       e.clientY < bounds.top + 10 ||
       e.clientY > bounds.bottom - 10
     ) {
-      textRef.current.style.background = `radial-gradient(circle 100px at ${x}px ${y}px, #aff,  #5040ac, #00000010)`;
-      // @ts-ignore
-      textRef.current.style['-webkit-background-clip'] = 'text';
-      // @ts-ignore
-      textRef.current.style['-webkit-text-fill-color'] = 'transparent';
+      applyStyles(
+        `radial-gradient(circle 100px at ${x}px ${y}px, #aff,  #5040ac, #00000010)`,
+        `text`,
+        `transparent`
+      );
       return;
     }
 
-    textRef.current.style.background = `radial-gradient(circle 200px at ${x}px ${y}px, #aff,  #5040ac, #00000011)`;
-    // @ts-ignore
-    textRef.current.style['-webkit-background-clip'] = 'text';
-    // @ts-ignore
-    textRef.current.style['-webkit-text-fill-color'] = 'transparent';
+    if (
+      e.clientX < bounds.left + 30 ||
+      e.clientX > bounds.right - 30 ||
+      e.clientY < bounds.top + 30 ||
+      e.clientY > bounds.bottom - 30
+    ) {
+      applyStyles(
+        `radial-gradient(circle 150px at ${x}px ${y}px, #aff,  #5040ac, #00000010)`,
+        `text`,
+        `transparent`
+      );
+      return;
+    }
+
+    applyStyles(
+      `radial-gradient(circle 200px at ${x}px ${y}px, #aff,  #5040ac, #00000011)`,
+      `text`,
+      `transparent`
+    );
   }
 
   return (
     <PageLayout title={`Evervault's Encrypted Card`}>
       <MetaLayout title={`Evervault's Encrypted Card | Craft`} />
-      <div className='border rounded-md border-gray-700 flex flex-col p-4 w-[500px] h-[700px] mx-auto'>
+      <div className='border rounded-md border-gray-700 flex flex-col p-4 sm:w-[380px] md:w-[500px] h-[700px] mx-auto'>
         <div
           className='rounded-md h-[540px] overflow-clip relative'
           onMouseMove={handleMoveMove}
@@ -105,12 +105,14 @@ const Card = () => {
           }}>
           <p
             ref={textRef}
-            className='text-md text-transparent select-none transition-all duration-500 ease-in-out '>
+            className='text-[10px] text-transparent select-none transition-all duration-500 ease-in-out
+              break-all
+            '>
             {text}
           </p>
 
           <div className='absolute pointer-events-none transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'>
-            <div className='flex items-center justify-center w-44 h-44 rounded-full m-auto bg-skin-primary opacity-80'>
+            <div className='flex items-center justify-center w-44 h-44 rounded-full m-auto bg-skin-primary opacity-90'>
               <p className='text-skin-secondary text-5xl italic font-semibold select-none font-secondary'>
                 Ness
               </p>
