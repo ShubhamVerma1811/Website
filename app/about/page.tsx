89f6e507b36daadefd8242f32a1dd026a1d4fb79
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2024-03-22 13:51:38 +0530

    feat: about me from sanity

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,50 @@
-import { About } from 'components/About';
+import { MDXClient } from 'components/MDXClient';
 import { PageLayout } from 'layouts';
+import { serialize } from 'next-mdx-remote/serialize';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeCodeTitles from 'rehype-code-titles';
+import rehypeResizeImage from 'rehype-image-resize';
+import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
+import { transformer } from 'services/image-transformer';
+import { getClient } from 'services/sanity-server';
 
-export default function AboutPage() {
+async function getData() {
+  const about = await getClient().fetch(`*[_type == "about"][0]`);
+
+  const mdxSource = await serialize(about.body, {
+    mdxOptions: {
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: 'wrap',
+            properties: {
+              className: 'anchor'
+            }
+          }
+        ],
+        rehypeCodeTitles,
+        [rehypeResizeImage, { transformer }]
+      ]
+    }
+  });
+
+  return {
+    mdxSource
+  };
+}
+
+export default async function AboutPage() {
+  const { mdxSource } = await getData();
+
   return (
     <PageLayout>
-      <About />
+      <div className='prose max-w-none text-lg text-skin-secondary prose-headings:scroll-m-20 prose-headings:font-secondary prose-headings:text-skin-secondary prose-a:text-skin-accent prose-strong:text-skin-secondary prose-em:text-skin-secondary prose-code:rounded-sm prose-code:text-skin-secondary prose-li:text-skin-secondary'>
+        <MDXClient mdxSource={mdxSource} />
+      </div>
     </PageLayout>
   );
 }
