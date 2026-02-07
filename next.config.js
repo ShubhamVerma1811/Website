598ad97d253dd15dbabd75922e6a15032a9450c8
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2026-02-07 10:12:28 +0530

    chore: add picsum image

--- next.config.js
+++ next.config.js
@@ -3,20 +3,21 @@
  **/
 
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    DOMAIN: process.env.DOMAIN,
-    SHOWBANNER: process.env.SHOWBANNER
-  },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
-      { protocol: 'https', hostname: 'cdn.sanity.io' },
-      { protocol: 'https', hostname: 'source.unsplash.com' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'i.scdn.co' }
-    ]
-  }
+	reactStrictMode: true,
+	env: {
+		DOMAIN: process.env.DOMAIN,
+		SHOWBANNER: process.env.SHOWBANNER,
+	},
+	images: {
+		remotePatterns: [
+			{ protocol: "https", hostname: "avatars.githubusercontent.com" },
+			{ protocol: "https", hostname: "cdn.sanity.io" },
+			{ protocol: "https", hostname: "source.unsplash.com" },
+			{ protocol: "https", hostname: "images.unsplash.com" },
+			{ protocol: "https", hostname: "fastly.picsum.photos" },
+			{ protocol: "https", hostname: "i.scdn.co" },
+		],
+	},
 };
 
 module.exports = nextConfig;
